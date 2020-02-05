d9763b16cf77651be9c940afa791d562e23177b2
James Colannino <crankycyclops@gmail.com>
2020-02-05 18:32:58 +0800

    more whitespace fixes

--- config/auth.php
+++ config/auth.php
@@ -2,138 +2,138 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Authentication Defaults
-    |--------------------------------------------------------------------------
-    |
-    | This option controls the default authentication "guard" and password
-    | reset options for your application. You may change these defaults
-    | as required, but they're a perfect start for most applications.
-    |
-    */
+	/*
+	|--------------------------------------------------------------------------
+	| Authentication Defaults
+	|--------------------------------------------------------------------------
+	|
+	| This option controls the default authentication "guard" and password
+	| reset options for your application. You may change these defaults
+	| as required, but they're a perfect start for most applications.
+	|
+	*/
 
-    'defaults' => [
-        'guard' => 'adminweb',
-        'passwords' => 'admins',
-    ],
+	'defaults' => [
+		'guard' => 'adminweb',
+		'passwords' => 'admins',
+	],
 
-    /*
-    |--------------------------------------------------------------------------
-    | Authentication Guards
-    |--------------------------------------------------------------------------
-    |
-    | Next, you may define every authentication guard for your application.
-    | Of course, a great default configuration has been defined for you
-    | here which uses session storage and the Eloquent user provider.
-    |
-    | All authentication drivers have a user provider. This defines how the
-    | users are actually retrieved out of your database or other storage
-    | mechanisms used by this application to persist your user's data.
-    |
-    | Supported: "session", "token", "passport"
-    |
-    */
+	/*
+	|--------------------------------------------------------------------------
+	| Authentication Guards
+	|--------------------------------------------------------------------------
+	|
+	| Next, you may define every authentication guard for your application.
+	| Of course, a great default configuration has been defined for you
+	| here which uses session storage and the Eloquent user provider.
+	|
+	| All authentication drivers have a user provider. This defines how the
+	| users are actually retrieved out of your database or other storage
+	| mechanisms used by this application to persist your user's data.
+	|
+	| Supported: "session", "token", "passport"
+	|
+	*/
 
-    'guards' => [
+	'guards' => [
 
-        'adminweb' => [
-            'driver' => 'session',
-            'provider' => 'admins',
-        ],
+		'adminweb' => [
+			'driver' => 'session',
+			'provider' => 'admins',
+		],
 
-        'adminapi' => [
-            'driver' => 'passport',
-            'provider' => 'admins',
-        ],
-    ],
+		'adminapi' => [
+			'driver' => 'passport',
+			'provider' => 'admins',
+		],
+	],
 
-    /*
-    |--------------------------------------------------------------------------
-    | User Providers
-    |--------------------------------------------------------------------------
-    |
-    | All authentication drivers have a user provider. This defines how the
-    | users are actually retrieved out of your database or other storage
-    | mechanisms used by this application to persist your user's data.
-    |
-    | If you have multiple user tables or models you may configure multiple
-    | sources which represent each model / table. These sources may then
-    | be assigned to any extra authentication guards you have defined.
-    |
-    | Supported: "database", "eloquent"
-    |
-    */
+	/*
+	|--------------------------------------------------------------------------
+	| User Providers
+	|--------------------------------------------------------------------------
+	|
+	| All authentication drivers have a user provider. This defines how the
+	| users are actually retrieved out of your database or other storage
+	| mechanisms used by this application to persist your user's data.
+	|
+	| If you have multiple user tables or models you may configure multiple
+	| sources which represent each model / table. These sources may then
+	| be assigned to any extra authentication guards you have defined.
+	|
+	| Supported: "database", "eloquent"
+	|
+	*/
 
-    'providers' => [
+	'providers' => [
 
-        'admins' => [
-            'driver' => 'eloquent',
-            'model' => App\Models\Admin::class,
-        ],
-    ],
+		'admins' => [
+			'driver' => 'eloquent',
+			'model' => App\Models\Admin::class,
+		],
+	],
 
-    /*
-    |--------------------------------------------------------------------------
-    | Resetting Passwords
-    |--------------------------------------------------------------------------
-    |
-    | You may specify multiple password reset configurations if you have more
-    | than one user table or model in the application and you want to have
-    | separate password reset settings based on the specific user types.
-    |
-    | The expire time is the number of minutes that the reset token should be
-    | considered valid. This security feature keeps tokens short-lived so
-    | they have less time to be guessed. You may change this as needed.
-    |
-    */
+	/*
+	|--------------------------------------------------------------------------
+	| Resetting Passwords
+	|--------------------------------------------------------------------------
+	|
+	| You may specify multiple password reset configurations if you have more
+	| than one user table or model in the application and you want to have
+	| separate password reset settings based on the specific user types.
+	|
+	| The expire time is the number of minutes that the reset token should be
+	| considered valid. This security feature keeps tokens short-lived so
+	| they have less time to be guessed. You may change this as needed.
+	|
+	*/
 
-    'passwords' => [
-        'users' => [
-            'provider' => 'users',
-            'table' => 'password_resets',
-            'expire' => 60,
-            'throttle' => 60,
-        ],
-    ],
+	'passwords' => [
+		'users' => [
+			'provider' => 'users',
+			'table' => 'password_resets',
+			'expire' => 60,
+			'throttle' => 60,
+		],
+	],
 
-    /*
-    |--------------------------------------------------------------------------
-    | Password Confirmation Timeout
-    |--------------------------------------------------------------------------
-    |
-    | Here you may define the amount of seconds before a password confirmation
-    | times out and the user is prompted to re-enter their password via the
-    | confirmation screen. By default, the timeout lasts for three hours.
-    |
-    */
+	/*
+	|--------------------------------------------------------------------------
+	| Password Confirmation Timeout
+	|--------------------------------------------------------------------------
+	|
+	| Here you may define the amount of seconds before a password confirmation
+	| times out and the user is prompted to re-enter their password via the
+	| confirmation screen. By default, the timeout lasts for three hours.
+	|
+	*/
 
-    'password_timeout' => 10800,
+	'password_timeout' => 10800,
 
-    /*
-    |--------------------------------------------------------------------------
-    | Oauth Configuration (via Laravel Passport)
-    |--------------------------------------------------------------------------
-    |
-    | Configuration options governing how Oauth is performed.
-    |
-    */
+	/*
+	|--------------------------------------------------------------------------
+	| Oauth Configuration (via Laravel Passport)
+	|--------------------------------------------------------------------------
+	|
+	| Configuration options governing how Oauth is performed.
+	|
+	*/
 
-    'oauth' => [
+	'oauth' => [
 
-        'expires_in' => [
+		'expires_in' => [
 
-            // How many hours tokens are good for until they expire. Default
-            // is one year (24 * 365 = 8760)
-            'tokens' => 8760,
+			// How many hours tokens are good for until they expire. Default
+			// is one year (24 * 365 = 8760)
+			'tokens' => 8760,
 
-            // How many hours refresh tokens are good for until they expire. Default
-            // is one year (24 * 365 = 8760)
-            'refresh_tokens' => 8760,
+			// How many hours refresh tokens are good for until they expire. Default
+			// is one year (24 * 365 = 8760)
+			'refresh_tokens' => 8760,
 
-            // How many hours access tokens are good for until they expire. Default
-            // is one year (24 * 365 = 8760)
-            'personal_access_tokens' => 8760,
-        ],
-    ],
+			// How many hours access tokens are good for until they expire. Default
+			// is one year (24 * 365 = 8760)
+			'personal_access_tokens' => 8760,
+		],
+	],
 ];
