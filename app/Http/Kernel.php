e2d9def664b1e9dcbf9370f503d67ee18dcd4c9d
James Colannino <crankycyclops@gmail.com>
2020-02-05 18:30:20 +0800

    whitespace fixes

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -4,102 +4,102 @@ namespace App\Http;
 
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
-class Kernel extends HttpKernel
-{
-    /**
-     * The application's global HTTP middleware stack.
-     *
-     * These middleware are run during every request to your application.
-     *
-     * @var array
-     */
-    protected $middleware = [
-        \App\Http\Middleware\TrustProxies::class,
-        \App\Http\Middleware\CheckForMaintenanceMode::class,
-        \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
-        \App\Http\Middleware\TrimStrings::class,
-        \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+class Kernel extends HttpKernel {
+
+	/**
+	 * The application's global HTTP middleware stack.
+	 *
+	 * These middleware are run during every request to your application.
+	 *
+	 * @var array
+	 */
+	protected $middleware = [
+		\App\Http\Middleware\TrustProxies::class,
+		\App\Http\Middleware\CheckForMaintenanceMode::class,
+		\Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
+		\App\Http\Middleware\TrimStrings::class,
+		\Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+	];
+
+	/**
+	 * The application's route middleware groups.
+	 *
+	 * @var array
+	 */
+	protected $middlewareGroups = [
+
+		// Middleware group for non-admin web routes.
+		'public' => [
+			\App\Http\Middleware\EncryptCookies::class,
+			\Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+			\Illuminate\Session\Middleware\StartSession::class,
+			// \Illuminate\Session\Middleware\AuthenticateSession::class,
+			\Illuminate\View\Middleware\ShareErrorsFromSession::class,
+			\App\Http\Middleware\VerifyCsrfToken::class,
+			\Illuminate\Routing\Middleware\SubstituteBindings::class,
+		],
+
+		// Middleware for admin web routes. The CreateFreshApiToken sets a token
+		// cookie that allows the admin to consume the admin api without
+		// explicitly sending an access token. The AuthenticateSession class,
+		// meanwhile, will forcibly logout an admin if that admin's password
+		// has changed mid-session.
+		'admin' => [
+			\App\Http\Middleware\EncryptCookies::class,
+			\Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+			\Illuminate\Session\Middleware\StartSession::class,
+			'adminauth:adminweb',
+			\Illuminate\Session\Middleware\AuthenticateSession::class,
+			\Illuminate\View\Middleware\ShareErrorsFromSession::class,
+			\App\Http\Middleware\VerifyCsrfToken::class,
+			\Illuminate\Routing\Middleware\SubstituteBindings::class,
+			\Laravel\Passport\Http\Middleware\CreateFreshApiToken::class
+		],
+
+		// Middleware for admin api routes.
+		'adminapi' => [
+			'throttle:60,1',
+			'adminauth:adminapi',
+			\Illuminate\Routing\Middleware\SubstituteBindings::class,
+		],
     ];
 
-    /**
-     * The application's route middleware groups.
-     *
-     * @var array
-     */
-    protected $middlewareGroups = [
+	/**
+	 * The application's route middleware.
+	 *
+	 * These middleware may be assigned to groups or used individually.
+	 *
+	 * @var array
+	 */
+	protected $routeMiddleware = [
+		'auth' => \App\Http\Middleware\Authenticate::class,
+		'adminauth' => \App\Http\Middleware\AdminAuthenticate::class,
+		'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+		'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
+		'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
+		'can' => \Illuminate\Auth\Middleware\Authorize::class,
+		'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
+		'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
+		'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
+		'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+		'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+	];
 
-        // Middleware group for non-admin web routes.
-        'public' => [
-            \App\Http\Middleware\EncryptCookies::class,
-            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-            \Illuminate\Session\Middleware\StartSession::class,
-            // \Illuminate\Session\Middleware\AuthenticateSession::class,
-            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-            \App\Http\Middleware\VerifyCsrfToken::class,
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
-        ],
-
-        // Middleware for admin web routes. The CreateFreshApiToken sets a token
-        // cookie that allows the admin to consume the admin api without
-        // explicitly sending an access token. The AuthenticateSession class,
-        // meanwhile, will forcibly logout an admin if that admin's password
-        // has changed mid-session.
-        'admin' => [
-            \App\Http\Middleware\EncryptCookies::class,
-            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-            \Illuminate\Session\Middleware\StartSession::class,
-            'adminauth:adminweb',
-            \Illuminate\Session\Middleware\AuthenticateSession::class,
-            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-            \App\Http\Middleware\VerifyCsrfToken::class,
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \Laravel\Passport\Http\Middleware\CreateFreshApiToken::class
-        ],
-
-        // Middleware for admin api routes.
-        'adminapi' => [
-            'throttle:60,1',
-            'adminauth:adminapi',
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
-        ],
-    ];
-
-    /**
-     * The application's route middleware.
-     *
-     * These middleware may be assigned to groups or used individually.
-     *
-     * @var array
-     */
-    protected $routeMiddleware = [
-        'auth' => \App\Http\Middleware\Authenticate::class,
-        'adminauth' => \App\Http\Middleware\AdminAuthenticate::class,
-        'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
-        'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
-        'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
-        'can' => \Illuminate\Auth\Middleware\Authorize::class,
-        'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
-        'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
-        'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
-        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-    ];
-
-    /**
-     * The priority-sorted list of middleware.
-     *
-     * This forces non-global middleware to always be in the given order.
-     *
-     * @var array
-     */
-    protected $middlewarePriority = [
-        \Illuminate\Session\Middleware\StartSession::class,
-        \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-        \App\Http\Middleware\Authenticate::class,
-        \App\Http\Middleware\AdminAuthenticate::class,
-        \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        \Illuminate\Session\Middleware\AuthenticateSession::class,
-        \Illuminate\Routing\Middleware\SubstituteBindings::class,
-        \Illuminate\Auth\Middleware\Authorize::class,
-    ];
+	/**
+	 * The priority-sorted list of middleware.
+	 *
+	 * This forces non-global middleware to always be in the given order.
+	 *
+	 * @var array
+	 */
+	protected $middlewarePriority = [
+		\Illuminate\Session\Middleware\StartSession::class,
+		\Illuminate\View\Middleware\ShareErrorsFromSession::class,
+		\App\Http\Middleware\Authenticate::class,
+		\App\Http\Middleware\AdminAuthenticate::class,
+		\Illuminate\Routing\Middleware\ThrottleRequests::class,
+		\Illuminate\Session\Middleware\AuthenticateSession::class,
+		\Illuminate\Routing\Middleware\SubstituteBindings::class,
+		\Illuminate\Auth\Middleware\Authorize::class,
+	];
 }
