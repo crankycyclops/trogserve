c845a479e69677fda6a57f1742de9b7a67f3144e
James Colannino <crankycyclops@gmail.com>
2020-02-06 17:46:38 +0800

    added source maps and versioning to frontend compilation

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,4 +13,10 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
 	.sass('resources/sass/app.scss', 'public/css')
-	.extract();
+	.extract()
+	.sourceMaps();
+
+// Version files in production for cache-busting purposes
+if (mix.inProduction()) {
+	mix.version();
+}
