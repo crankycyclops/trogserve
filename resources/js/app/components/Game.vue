92aa582074b3af3628d0278a18864a2d2c705cbd
James Colannino <crankycyclops@gmail.com>
2020-04-18 16:49:59 -0700

    built game ui for the frontend

--- resources/js/app/components/Game.vue
+++ resources/js/app/components/Game.vue
@@ -2,19 +2,76 @@
 
 	<v-card>
 
-		<v-card-title>{{ title }}</v-card-title>
-
-		<v-card-subtitle>
-			TODO
-		</v-card-subtitle>
-
 		<v-card-text>
-			TODO
+
+			<div id="output"></div>
+
+			<v-row>
+
+				<v-col cols="12">
+					<v-text-field
+						v-model="command"
+						label="What do you do now?"
+						append-outer-icon="send"
+						clear-icon="clear"
+						clearable
+						@keyup.enter="send"
+						@click:append-outer="send"
+						@click:clear="clear"
+					/>
+				</v-col>
+
+			</v-row>
+
 		</v-card-text>
 
 	</v-card>
 
 </template>
+
+<style scoped>
+
+	@media only screen and (max-height: 550px) {
+
+		#output {
+			height: 47vh;
+		}
+	}
+
+	@media only screen and (min-height: 551px) and (max-height: 699px) {
+
+		#output {
+			height: 55vh;
+		}
+	}
+
+	@media only screen and (min-height: 700px) {
+
+		#output {
+			height: 65vh;
+		}
+	}
+
+	#output {
+		background-color: #2a2a2a;
+		overflow-y: auto;
+		scrollbar-color: #d0d0d0 #505050;
+		scrollbar-width: auto;
+	}
+
+	#output::-webkit-scrollbar {
+		width: 11px;
+	}
+
+	#output::-webkit-scrollbar-track {
+		background: #505050;
+	}
+
+	#output::-webkit-scrollbar-thumb {
+		background: #d0d0d0;
+	}
+
+</style>
 
 <script>
 
@@ -28,12 +85,29 @@
 		data: function () {
 
 			return {
-				//
+
+				// The text currently displayed in the command input field
+				command: ''
 			};
 		},
 
 		methods: {
-			//
+
+			// Clears the command input field
+			clear: function () {
+
+				this.command = '';
+			},
+
+			// Sends command to the game server
+			send: function () {
+
+				if (this.command.length) {
+
+					// TODO
+					this.clear();
+				}
+			}
 		}
 	};
 
