093d611bf4fb8ea9c03f964bfcd445155395d337
James Colannino <james@colannino.dev>
2021-03-19 19:15:46 -0700

    added game destruction to the games list page

--- resources/js/admin/components/Games.vue
+++ resources/js/admin/components/Games.vue
@@ -17,6 +17,14 @@
 
 				<message type="error" :message="games.error" />
 
+				<game-crud
+					:id="crud.gameId"
+					:confirmDestroy="crud.showDestroyDialog"
+					@destroy="onDestroy"
+					@cancel="onCancelCrud"
+					@error="onErrorCrud"
+				/>
+
 				<!-- Clickable list of games -->
 				<v-text-field
 					v-model="table.search"
@@ -34,6 +42,7 @@
 				>
 
 					<template v-slot:item.actions="{ item }">
+						<v-icon small @click="destroy(item.id)">delete</v-icon>
 						<v-icon small @click="viewGame(item.id)">view_list</v-icon>
 					</template>
 
@@ -85,6 +94,8 @@
 
 	import Message from './ui/Message';
 	import Progress from './ui/Progress';
+
+	import GameCrud from './crud/Game';
 	import RequestMixin from '../mixins/Request.vue';
 
 	export default {
@@ -124,6 +135,16 @@
 
 					// This is the data that's returned from the API call.
 					data: []
+				},
+
+				crud: {
+
+					// If we're deleting or editing a game, this is the id
+					// that gets passed into the CRUD component
+					gameId: 0,
+
+					// Toggles the game destruction confirmation dialog
+					showDestroyDialog: false
 				}
 			};
 		},
@@ -171,12 +192,52 @@
 					.finally(() => {
 						this.games.loading = false;
 					});
+			},
+
+			// Initiate a destroy operation pending confirmation
+			destroy(id) {
+
+				this.crud.gameId = id;
+				this.crud.showDestroyDialog = true;
+			},
+
+			// Called when a game is successfully destroyed
+			onDestroy(id) {
+
+				// TODO: display confirmation message
+				this.crud.showDestroyDialog = false;
+				this.loadGames();
+			},
+
+			// Called when a user cancels a CRUD operation
+			onCancelCrud(type, id) {
+
+				switch (type) {
+
+					case 'destroy':
+						this.crud.showDestroyDialog = false;
+						break;
+
+					default:
+						break;
+				};
+			},
+
+			// Called when a CRUD operation results in an error
+			onErrorCrud(type, id, message) {
+
+				this.games.error = message;
+
+				setTimeout(() => {
+					this.games.error = '';
+				}, 5000);
 			}
 		},
 
 		components: {
 			'message': Message,
-			'progress-bar': Progress
+			'progress-bar': Progress,
+			'game-crud': GameCrud
 		},
 
 		mixins: [
